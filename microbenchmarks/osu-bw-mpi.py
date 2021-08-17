43ed5bc0473254c018a401e3002168f7d0e31df5
Zane Fink <finkzane@gmail.com>
2021-08-17 09:50:07 -0700

    per-iteration data output

--- microbenchmarks/osu-bw-mpi.py
+++ microbenchmarks/osu-bw-mpi.py
@@ -4,9 +4,11 @@ import numpy as np
 import array
 import pandas as pd
 import sys
+import random
 
 output_df = None
 
+RANDOM_SEED = 42
 LOW_ITER_THRESHOLD = 8192
 WARMUP_ITERS = 10
 
@@ -43,23 +45,52 @@ def main():
 
     iter_params = []
     msg_size = min_msg_size
+    total_iterations = 0
     while msg_size <= max_msg_size:
         if msg_size <= LOW_ITER_THRESHOLD:
             iter = low_iter
         else:
             iter = high_iter
+        total_iterations += iter + WARMUP_ITERS
         iter_params.append((iter, msg_size))
         msg_size *= 2
-    datarange = (min_msg_size, max_msg_size)
-    for iter, msg_size in iter_params:
-        comm.barrier()
-        do_iteration(comm, rank, msg_size, windows, iter, iter_datafile_base, datarange)
 
-def do_iteration(comm, rank, message_size, windows, num_iters, iter_datafile_base, datarange):
+    if rank == 0:
+        iteration_data = np.ndarray((total_iterations, 5), dtype=np.float64)
+    else:
+        iteration_data = None
+
+    for iter, msg_size in iter_params:
+        do_iteration(comm, rank, msg_size, windows, iter, iteration_data)
+    if rank == 0:
+        write_output(iter_datafile_base, iteration_data, windows)
+    sys.exit(0)
+
+
+def write_output(filename_base, iteration_data, windows):
+    import pandas as pd
+    header = ("Message size", "Iteration", "Total Iterations",
+              "Warmup", "Bandwidth (MB/s)"
+              )
+    df = pd.DataFrame(iteration_data, columns=header)
+    time = df['Bandwidth (MB/s)']
+    data_volume = df['Message size'] * windows / 1e6
+    bw = data_volume / time
+    df['Bandwidth (MB/s)'] = bw
+
+    from datetime import datetime
+    now = datetime.now()
+    dt_string = now.strftime("%Y_%m_%d_%H_%M_%S")
+    output_filename = f"{dt_string}_{filename_base}.csv"
+    with open(output_filename, 'w') as of:
+        of.write('# ' + ' '.join(sys.argv) + '\n')
+        df.to_csv(of, index=False)
+
+
+def do_iteration(comm, rank, message_size, windows, num_iters, iter_data=None):
     local_data = np.ones(message_size, dtype='int8')
     remote_data = np.ones(message_size, dtype='int8')
     ack = np.zeros(1, dtype='int8')
-    t_data = np.zeros(num_iters+WARMUP_ITERS, dtype='float64')
 
     partner_idx = not rank
     am_low = rank == 0
@@ -67,10 +98,12 @@ def do_iteration(comm, rank, message_size, windows, num_iters, iter_datafile_bas
     tstart = 0
 
     for idx in range(num_iters + WARMUP_ITERS):
+        iter_start = time.perf_counter()
         requests = [MPI.REQUEST_NULL for _ in range(windows)]
         if idx == WARMUP_ITERS:
-            tstart = time.time()
-        tst = time.perf_counter()
+            comm.barrier()
+            iter_start = time.perf_counter()
+            tstart = time.perf_counter()
         if am_low:
             for window in range(windows):
                 requests[window] = comm.Isend(local_data, tag=0, dest=partner_idx)
@@ -81,28 +114,23 @@ def do_iteration(comm, rank, message_size, windows, num_iters, iter_datafile_bas
                 requests[window] = comm.Irecv(remote_data, tag=0, source=partner_idx)
             MPI.Request.Waitall(requests)
             comm.Send(ack, tag=1, dest=partner_idx)
-        tend = time.perf_counter()
-        t_data[idx] = tend-tst
-    elapsed_time = time.time() - tstart
+        iter_end = time.perf_counter()
+        if iter_data is not None:
+            iter_data[do_iteration.iter_count] = (message_size,
+                                                  idx,
+                                                  num_iters,
+                                                  WARMUP_ITERS,
+                                                  iter_end - iter_start
+                                                  )
+            do_iteration.iter_count += 1
+    elapsed_time = time.perf_counter() - tstart
     if am_low:
-        global output_df
         display_iteration_data(rank, elapsed_time, num_iters, windows, message_size)
-        # iter_filename = iter_datafile_base + str(rank) + '.csv'
-        # iter_data = write_iteration_data(rank, num_iters, windows, message_size, t_data)
-        # if output_df is None:
-        #     output_df = iter_data
-        # else:
-        #     output_df = pd.concat([output_df, iter_data])
-        # if message_size == datarange[1]:
-        #     print("Writing data now!")
-        #     from datetime import datetime
-        #     now = datetime.now()
-        #     dt_string = now.strftime("%Y_%m_%d_%H_%M_%S")
-        #     iter_filename = f"{dt_string}_{iter_filename}"
+    comm.barrier()
 
-        #     with open(iter_filename, 'w') as of:
-        #         of.write('# ' + ' '.join(sys.argv) + '\n')
-        #         output_df.to_csv(of, index=False)
+
+do_iteration.iter_count = 0
+
 
 def write_iteration_data(rank, num_iters, windows, message_size, timing_data):
    header = ("Process,Msg Size, Iteration, Bandwidth (MB/s)")
