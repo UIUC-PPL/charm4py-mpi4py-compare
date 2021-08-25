75021539a114c36798e120a69d4132f0e48865eb
Zane Fink <finkzane@gmail.com>
2021-08-25 17:06:54 -0500

    strong scaling script

---
+++ scripts/stencil/submit_strong.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --nodes=16
+#SBATCH -J jacobi2d_strong
+#SBATCH --tasks-per-node=48
+#SBATCH --time=1:35:00
+#SBATCH --partition=skx-normal
+
+source ~/.bashrc
+
+python3 strong_scaling.py
+
