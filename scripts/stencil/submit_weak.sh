61e51342834c0b071f65c5f268cf1ef4e23ea98f
Zane Fink <finkzane@gmail.com>
2021-08-26 11:21:32 -0500

    script to submit weak scaling results

---
+++ scripts/stencil/submit_weak.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --nodes=16
+#SBATCH -J jacobi2d_weak
+#SBATCH --tasks-per-node=48
+#SBATCH --time=2:35:00
+#SBATCH --partition=skx-normal
+
+source ~/.bashrc
+
+python3 weak_scaling.py
+
