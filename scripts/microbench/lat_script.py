c69c04367eec1e5915c91f8f12a60d85ae2d545b
Zane Fink <finkzane@gmail.com>
2021-08-27 17:20:22 -0500

    Make it for latency

--- scripts/microbench/lat_script.py
+++ scripts/microbench/lat_script.py
@@ -13,34 +13,33 @@ def main():
 
     charm4py_intra_cmd = ['python3',
                           '/home1/08302/tg876011/charm-mpi-compare/microbenchmarks/osu-lat-charm.py',
-                          '1', '4194304', '1000', '500', '0',
-                          'charm_lat_intrasocket'
+                          '1', '4194304', '1000', '500', '0'
                           ]
 
     # On stampede Core 1 is on NUMA node 1
     charm4py_inter_cmd = ['python3',
                           '/home1/08302/tg876011/charm-mpi-compare/microbenchmarks/osu-lat-charm.py',
-                          '1', '4194304', '1000', '500', '0',
-                          'charm_lat_intersocket'
+                          '1', '4194304', '1000', '500', '0'
                           ]
 
 
     mpi4py_intra_cmd = ['python3',
                         '/home1/08302/tg876011/charm-mpi-compare/microbenchmarks/osu-lat-mpi.py',
-                        '1', '4194304', '1000', '500', '0',
-                        'mpi_lat_intrasocket'
+                        '1', '4194304', '1000', '500', '0'
                         ]
     mpi4py_inter_cmd = ['python3',
                         '/home1/08302/tg876011/charm-mpi-compare/microbenchmarks/osu-lat-mpi.py',
-                        '1', '4194304', '1000', '500', '0',
-                        'mpi_lat_intersocket'
+                        '1', '4194304', '1000', '500', '0'
                         ]
+    mpi_inter_cmd = ['/home1/08302/tg876011/osu-micro-benchmarks/mpi/pt2pt']
+    mpi_intra_cmd = ['/home1/08302/tg876011/osu-micro-benchmarks/mpi/pt2pt']
 
-    mpirun_base = ['-np','2']
-    srun_base = ['--ntasks=2']
+    charm_inter_cmd = ['/home1/08302/tg876011/charm_lat']
+    charm_intra_cmd = ['/home1/08302/tg876011/charm_lat']
+
+    mpirun_base = ['-np', '2']
 
     mpirun = sh.Command('mpirun')
-    srun = sh.Command('srun')
 
     intra_mpirun_args = (*mpirun_base, *intra_mpirun)
     inter_mpirun_args = (*mpirun_base, *inter_mpirun)
@@ -51,18 +50,30 @@ def main():
     mpi4py_inter = mpirun.bake(*inter_mpirun_args, *mpi4py_inter_cmd)
     mpi4py_inter._output_f = open('mpi4py_intersocket_lat.csv', 'w')
 
-    charm_base = srun.bake(*srun_base)
-
     charm4py_intra = charm_base.bake(*charm4py_intra_cmd, *intra_charmrun)
     charm4py_intra._output_f = open('charm4py_intrasocket_lat.csv', 'w')
 
     charm4py_inter = charm_base.bake(*charm4py_inter_cmd, *inter_charmrun)
     charm4py_inter._output_f = open('charm4py_intersocket_lat.csv', 'w')
 
+    charm_inter = charm_base.bake(*charm_inter_cmd, *inter_charmrun)
+    charm_inter._output_f = open('charm_intersocket_lat.csv', 'w')
+
+    charm_intra = charm_base.bake(*charm_intra_cmd, *intra_charmrun)
+    charm_intra._output_f = open('charm_intrasocket_lat.csv', 'w')
+
+    mpi_inter = mpirun.bake(*inter_mpirun_args, *mpi_inter_cmd)
+    mpi_inter._output_f = open('mpi_intersocket_lat.csv', 'w')
+
+    mpi_intra = mpirun.bake(*intra_mpirun_args, *mpi_intra_cmd)
+    mpi_intra._output_f = open('mpi_intrasocket_lat.csv', 'w')
+
 
     cmds = [
         mpi4py_intra, mpi4py_inter,
-        charm4py_inter, charm4py_intra
+        charm4py_inter, charm4py_intra,
+        charm_inter, charm_intra,
+        mpi_inter, mpi_intra
     ]
 
     for i in range(10):
