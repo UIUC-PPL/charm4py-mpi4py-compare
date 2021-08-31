098f9bd104bd597e63595bc972795750076bb0ad
Zane Fink <finkzane@gmail.com>
2021-08-31 16:48:07 -0500

    GPU latency script

--- scripts/microbench/lat_script_gpu.py
+++ scripts/microbench/lat_script_gpu.py
@@ -3,93 +3,103 @@ import sys
 import random
 import time
 import os
+import sys
 
 
 def main():
-    os.putenv('I_MPI_EAGER_THRESHOLD','8192')
-    intra_mpirun = ['--map-by', 'core', '--bind-to', 'core' ]
-    inter_mpirun = ['--map-by', 'socket', '--bind-to', 'core']
+    hostname = sys.argv[1]
+    os.putenv('UCX_RNDV_THRESH', '131072')
+    os.putenv('UCX_MEMTYPE_CACHE', 'n')
+    mpirun_args = ['-H', f'{hostname}:2', '-x', 'PATH', '-x', 'LD_LIBRARY_PATH',
+                   '-x', 'UCX_RNDV_THRESH=131072', '-x', 'UCX_MEMTYPE_CACHE=n',
+                   ]
 
-    intra_charmrun = ['+pemap', 'L0,2', '+no_isomalloc_sync']
-    inter_charmrun = ['+pemap', 'L0,1', '+no_isomalloc_sync']
+    charm_args = ['+ppn', '1' '+pemap', 'L0,4']
 
-    charm4py_intra_cmd = ['python3',
-                          '/home1/08302/tg876011/charm-mpi-compare/microbenchmarks/osu-lat-charm.py',
-                          '1', '4194304', '1000', '500', '0'
+    # UCX_RNDV_THRESH=131072 UCX_MEMTYPE_CACHE=n jsrun -n2 -a1 -c2 -g1 -K2 -r2 --smpiargs="-disable_gpu_hooks" python3 $PWD/osu-lat-gpu-charm.py +ppn 1 +pemap L0,4 $((1<<10)) $((1<<23)) 1000 100 0 1
+    charm4py_cmd_small = ['python3',
+                          '/ccs/home/zanef2/charm-mpi-compare/microbench/osu-lat-gpu-charm.py',
+                          '1', '1024', '1000', '500', '0', '1'
+                          ]
+    charm4py_cmd_large = ['python3',
+                          '/ccs/home/zanef2/charm-mpi-compare/microbench/osu-lat-gpu-charm.py',
+                          '2048', '4194304', '1000', '500', '0', '1'
                           ]
 
-    # On stampede Core 1 is on NUMA node 1
-    charm4py_inter_cmd = ['python3',
-                          '/home1/08302/tg876011/charm-mpi-compare/microbenchmarks/osu-lat-charm.py',
-                          '1', '4194304', '1000', '500', '0'
+
+    mpi4py_cmd_small = ['python3',
+                        '/ccs/home/zanef2/charm-mpi-compare/microbench/osu-lat-gpu-mpi.py',
+                        '1', '1024', '1000', '500', '0', '1'
+                          ]
+    mpi4py_cmd_large = ['python3',
+                        '/ccs/home/zanef2/charm-mpi-compare/microbench/osu-lat-gpu-mpi.py',
+                        '2048', '4194304', '1000', '500', '0', '1'
                           ]
 
-
-    mpi4py_intra_cmd = ['python3',
-                        '/home1/08302/tg876011/charm-mpi-compare/microbenchmarks/osu-lat-mpi.py',
-                        '1', '4194304', '1000', '500', '0'
-                        ]
-    mpi4py_inter_cmd = ['python3',
-                        '/home1/08302/tg876011/charm-mpi-compare/microbenchmarks/osu-lat-mpi.py',
-                        '1', '4194304', '1000', '500', '0'
-                        ]
-    mpi_inter_cmd = ['/home1/08302/tg876011/osu-micro-benchmarks/mpi/pt2pt/osu_latency']
-    mpi_intra_cmd = ['/home1/08302/tg876011/osu-micro-benchmarks/mpi/pt2pt/osu_latency']
-
-    charm_inter_cmd = ['/home1/08302/tg876011/charm_lat/latency']
-    charm_intra_cmd = ['/home1/08302/tg876011/charm_lat/latency']
+    mpi_cmd = ['/ccs/home/zanef2/osu-micro-benchmarks-5.8/build/install/libexec/osu-micro-benchmarks/mpi/pt2pt/osu_latency', 'D', 'D'
+               ]
+    charm_cmd = ['/ccs/home/zanef2/charm_lat/latency', '-z']
 
     mpirun_base = ['-np', '2']
+    jsrun_base = ['-n2', '-a1', '-c1', '-g1', '-K2', '-r2', '--smpiargs="-disable_gpu_hooks"']
 
     mpirun = sh.Command('mpirun')
+    jsrun = sh.Command('jsrun')
 
-    intra_mpirun_args = (*mpirun_base, *intra_mpirun)
-    inter_mpirun_args = (*mpirun_base, *inter_mpirun)
+    mpirun_args = (*mpirun_base, *mpirun_args)
 
-    mpi4py_intra = mpirun.bake(*intra_mpirun_args, *mpi4py_intra_cmd)
-    mpi4py_intra._output_f = open('mpi4py_intrasocket_lat.csv', 'w')
+    mpi4py_small = mpirun.bake(*mpirun_args, *mpi4py_cmd_small)
+    mpi4py_large = mpirun.bake(*mpirun_args, *mpi4py_cmd_large)
+    mpi4py_small._output_f = open('mpi4py_intranode_gpu_lat.csv', 'w')
+    mpi4py_large._output_f = mpi4py_large._output_f
 
-    mpi4py_inter = mpirun.bake(*inter_mpirun_args, *mpi4py_inter_cmd)
-    mpi4py_inter._output_f = open('mpi4py_intersocket_lat.csv', 'w')
+    charm4py_small = jsrun.bake(*charm4py_cmd_small, *charm_args)
+    charm4py_large = jsrun.bake(*charm4py_cmd_large, *charm_args)
+    charm4py_small._output_f = open('charm4py_intranode_gpu_lat.csv', 'w')
+    charm4py_large._output_f = small_intra._output_f
 
-    charm4py_intra = mpirun.bake(*charm4py_intra_cmd, *intra_charmrun)
-    charm4py_intra._output_f = open('charm4py_intrasocket_lat.csv', 'w')
+    charm_intra = jsrun.bake(*charm_cmd, *charm_args)
+    charm_intra._output_f = open('charm_intranode_gpu_lat.csv', 'w')
 
-    charm4py_inter = mpirun.bake(*charm4py_inter_cmd, *inter_charmrun)
-    charm4py_inter._output_f = open('charm4py_intersocket_lat.csv', 'w')
-
-    charm_inter = mpirun.bake(*charm_inter_cmd, *inter_charmrun)
-    charm_inter._output_f = open('charm_intersocket_lat.csv', 'w')
-
-    charm_intra = mpirun.bake(*charm_intra_cmd, *intra_charmrun)
-    charm_intra._output_f = open('charm_intrasocket_lat.csv', 'w')
-
-    mpi_inter = mpirun.bake(*inter_mpirun_args, *mpi_inter_cmd)
-    mpi_inter._output_f = open('mpi_intersocket_lat.csv', 'w')
-
-    mpi_intra = mpirun.bake(*intra_mpirun_args, *mpi_intra_cmd)
-    mpi_intra._output_f = open('mpi_intrasocket_lat.csv', 'w')
+    mpi_intra = mpirun.bake(*mpirun_args, *mpi_cmd)
+    mpi_intra._output_f = open('mpi_intranode_gpu_lat.csv', 'w')
 
 
     cmds = [
-        mpi4py_intra, mpi4py_inter,
-        charm4py_inter, charm4py_intra,
-        charm_inter, charm_intra,
-        mpi_inter, mpi_intra
+        (mpi4py_small, mpi4py_large),
+        (charm4py_small, charm4py_large),
+        charm_intra,
+        mpi_intra
     ]
 
     for i in range(10):
         random.shuffle(cmds)
         for idx, c in enumerate(cmds):
             t_start = time.time()
-            cmd_str = str(c)
-            print(f"Executing command: {c}")
-            # flush because writing to the file behaves differently than
-            # redirecting to it, can reorder the output
-            c._output_f.write(f"# {cmd_str}\n")
-            c._output_f.flush()
-            c(_out=c._output_f, _err=c._output_f)
-            t_end = time.time()
+            if type(c) == tuple:
+                cmd_str = str(c[0])
+                print(f"Executing command: {c[0]}")
+                # flush because writing to the file behaves differently than
+                # redirecting to it, can reorder the output
+                c[0]._output_f.write(f"# {cmd_str}\n")
+                c[0]._output_f.flush()
+                c[0](_out=c[0]._output_f, _err=c[0]._output_f)
+
+                cmd_str = str(c[1])
+                c[1]._output_f.write(f"# {c[1]md_str}\n")
+                c[1]._output_f.flush()
+                c[1](_out=c[1]._output_f, _err=c[1]._output_f)
+                t_end = time.time()
+
+            else:
+                cmd_str = str(c)
+                print(f"Executing command: {c}")
+                # flush because writing to the file behaves differently than
+                # redirecting to it, can reorder the output
+                c._output_f.write(f"# {cmd_str}\n")
+                c._output_f.flush()
+                c(_out=c._output_f, _err=c._output_f)
+                t_end = time.time()
             print(f"Command {(i*len(cmds))+idx+1} of {10*len(cmds)} completed in {t_end - t_start}s.")
 
     for c in cmds:
