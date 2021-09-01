dacea0c830935170c81ad3bad577f91c6d49dc73
Zane Fink <finkzane@gmail.com>
2021-09-01 17:41:01 -0500

    Add other packing kernels

--- stencil/gpu_kernels.py
+++ stencil/gpu_kernels.py
@@ -11,6 +11,7 @@ def set_block_params(width, height):
     BLOCK_WIDTH = width
     BLOCK_HEIGHT = height
 
+
 @cuda.jit(device=True)
 def index(x, y):
     return x*BLOCK_WIDTH + y
@@ -21,6 +22,7 @@ def _pack_left(temperature, ghost):
     x = cuda.blockDim.x*cuda.blockIdx.x+cuda.threadIdx.x
     if x < BLOCK_HEIGHT:
           ghost[x] = temperature[index(x+1, 0)]
+
 
 @cuda.jit
 def _pack_right(temperature, ghost):
@@ -42,27 +44,73 @@ def _pack_bottom(temperature, ghost):
     if y < BLOCK_WIDTH:
           ghost[y] = temperature[index(BLOCK_HEIGHT-1, y+1)]
 
+
+@cuda.jit
+def _unpack_left(temperature, ghost):
+    x = cuda.blockDim.x*cuda.blockIdx.x+cuda.threadIdx.x
+    if x < BLOCK_HEIGHT:
+        temperature[index(x+1, 0)] = ghost[x]
+
+@cuda.jit
+def _unpack_right(temperature, ghost):
+    x = cuda.blockDim.x*cuda.blockIdx.x+cuda.threadIdx.x
+    if x < BLOCK_HEIGHT:
+        temperature[index(x+1, BLOCK_WIDTH-1)] = ghost[x]
+
+
+@cuda.jit
+def _unpack_top(temperature, ghost):
+    y = cuda.blockDim.x*cuda.blockIdx.x+cuda.threadIdx.x
+    if y < BLOCK_WIDTH:
+          temperature[index(0, y+1)] = ghost[y]
+
+
+@cuda.jit
+def _unpack_bottom(temperature, ghost):
+    y = cuda.blockDim.x*cuda.blockIdx.x+cuda.threadIdx.x
+    if y < BLOCK_WIDTH:
+        temperature[index(BLOCK_HEIGHT-1, y+1)] = ghost[y]
+
+
 def pack_left(temperature, ghost, stream=cuda.default_stream()):
     block_dim = (TILE_SIZE, 1)
     grid_dim = ((BLOCK_HEIGHT+(block_dim[0]-1))//block_dim[0], 1)
     _pack_left[grid_dim, block_dim, stream](temperature, ghost)
 
-# @cuda.jit
-# def unpack_left(temperature, ghost):
-#     for x in range(BLOCK_HEIGHT):
-#         temperature[index(x+1, 0)] = ghost[x]
+def pack_right(temperature, ghost, stream=cuda.default_stream()):
+    block_dim = (TILE_SIZE, 1)
+    grid_dim = ((BLOCK_HEIGHT+(block_dim[0]-1))//block_dim[0], 1)
+    _pack_right[grid_dim, block_dim, stream](temperature, ghost)
 
-# @cuda.jit
-# def unpack_right(temperature, ghost):
-#     for x in range(BLOCK_HEIGHT):
-#         temperature[index(x+1,BLOCK_WIDTH-1)] = ghost[x]
+def pack_top(temperature, ghost, stream=cuda.default_stream()):
+    block_dim = (TILE_SIZE, 1)
+    grid_dim = ((BLOCK_WIDTH+(block_dim[0]-1))//block_dim[0], 1)
+    _pack_top[grid_dim, block_dim, stream](temperature, ghost)
 
-# @cuda.jit
-# def unpack_top(temperature, ghost):
-#     for y in range(BLOCK_WIDTH):
-#         temperature[index(0, y+1)] = ghost[y]
+def pack_bottom(temperature, ghost, stream=cuda.default_stream()):
+    block_dim = (TILE_SIZE, 1)
+    grid_dim = ((BLOCK_WIDTH+(block_dim[0]-1))//block_dim[0], 1)
+    _pack_bottom[grid_dim, block_dim, stream](temperature, ghost)
 
-# @cuda.jit
-# def unpack_bottom(temperature, ghost):
-#     for y in range(BLOCK_WIDTH):
-#         temperature[index(BLOCK_HEIGHT-1,y+1)] = ghost[y]
+
+def unpack_left(temperature, ghost, stream=cuda.default_stream()):
+    block_dim = (TILE_SIZE, 1)
+    grid_dim = ((BLOCK_HEIGHT+(block_dim[0]-1))//block_dim[0], 1)
+    _unpack_left[grid_dim, block_dim, stream](temperature, ghost)
+
+def unpack_right(temperature, ghost, stream=cuda.default_stream()):
+    block_dim = (TILE_SIZE, 1)
+    grid_dim = ((BLOCK_HEIGHT+(block_dim[0]-1))//block_dim[0], 1)
+    _unpack_right[grid_dim, block_dim, stream](temperature, ghost)
+
+def unpack_top(temperature, ghost, stream=cuda.default_stream()):
+    block_dim = (TILE_SIZE, 1)
+    grid_dim = ((BLOCK_WIDTH+(block_dim[0]-1))//block_dim[0], 1)
+    _unpack_top[grid_dim, block_dim, stream](temperature, ghost)
+
+def unpack_bottom(temperature, ghost, stream=cuda.default_stream()):
+    block_dim = (TILE_SIZE, 1)
+    grid_dim = ((BLOCK_WIDTH+(block_dim[0]-1))//block_dim[0], 1)
+    _unpack_bottom[grid_dim, block_dim, stream](temperature, ghost)
+
+
